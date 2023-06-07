db640cb519c4a0f02be2884549be65d68d922d2c
shivamsinghrajputcse <shivamsinghcse21@gmail.com>
2023-06-08 01:16:41 +0530

    making it more stuctured by breaking into files

---
+++ calculator/script.js
@@ -0,0 +1,28 @@
+function add(){ 
+    let num1 = document.getElementById('num1').value;
+    let number1 = parseFloat(num1);
+    let num2 = document.getElementById('num2').value;
+    let number2 = parseFloat(num2);
+        document.getElementById('result').innerHTML=number1 + number2;
+    } 
+    function sub(){ 
+    let num1 = document.getElementById('num1').value;
+    let number1 = parseFloat(num1);
+    let num2 = document.getElementById('num2').value;
+    let number2 = parseFloat(num2);
+        document.getElementById('result').innerHTML=number1 - number2;
+    } 
+    function mul(){ 
+    let num1 = document.getElementById('num1').value;
+    let number1 = parseFloat(num1);
+    let num2 = document.getElementById('num2').value;
+    let number2 = parseFloat(num2);
+        document.getElementById('result').innerHTML=number1 * number2;
+    } 
+    function divi(){ 
+    let num1 = document.getElementById('num1').value;
+    let number1 = parseFloat(num1);
+    let num2 = document.getElementById('num2').value;
+    let number2 = parseFloat(num2);
+        document.getElementById('result').innerHTML=number1 / number2;
+    } 
